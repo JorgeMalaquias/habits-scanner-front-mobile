bc6c740993d0ed1636f30fa7cba500be35497138
jorge <jorgemalaquia@gmail.com>
2023-02-03 21:55:08 -0300

    feat: adding new font and prevent render bug

--- App.tsx
+++ App.tsx
@@ -1,20 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Inter_100Thin,
+  Inter_200ExtraLight,
+  Inter_300Light,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  Inter_900Black
+} from '@expo-google-fonts/inter';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Inter_100Thin,
+    Inter_200ExtraLight,
+    Inter_300Light,
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+    Inter_900Black
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View style={styles.container}>
+        <Loading />
+      </View>
+
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text>My first mobile app</Text>
+      <Text style={styles.text}>My first mobile app</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    color:'white',
+  }
 });
---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+import { ActivityIndicator, View } from "react-native";
+
+
+
+export function Loading() {
+    return (
+        
+            <ActivityIndicator color="#7C3AED" />
+       
+    );
+}
